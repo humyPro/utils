227cb41c53c7cd1753ed1a60a3d62ad78b387cd1
humyPro <39785161+humyPro@users.noreply.github.com>
2018-12-13 17:33:31 +0800

    可以设置忽略字段，需要深入检测的字段的类的基础包名

--- CheckFields.java
+++ CheckFields.java
@@ -1,38 +1,72 @@
-package com.humy.util;
+package com.humy.utils;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collection;
 
-/** 业务参数检测工具
- * @Author humy
- * @Date 2018/12/12 - 23:16
+/**
+ * @Auther: Hu Min
+ * @Date: 2018/12/10 15:44
+ * @Description:
  */
-public class CheckFields {
-    
+public class CheckFileds {
     /**
-     * 对象的类的默认的基础包名
+     * 判断一个model中的字段有没有空的,以最基本的model传入
+     *
+     * @param t
+     * @return 如果有空的字段，返回true
      */
-    public static String defaultBasePackageName="com.humy";
-    /**
-     * 传入一个对象，检查这个对象中有没有空值,一般用于检测javabean对象
-     * @param t 需要被检测的对象,默认包名可以自定义
-     * @return
-     */
-    public static <T> boolean checkFieldSEmpty(T t) {
-        return checkFieldSEmpty(t, defaultBasePackageName);
+    public static boolean checkFiledsIsEmpty(Object t) {
+        String defaultBasePackageName = "com.humy";
+        return checkFiledsIsEmpty(t, defaultBasePackageName);
     }
 
     /**
-     * 传入一个对象，检查这个对象中有没有空值,一般用于检测javabean对象
+     * 判断一个model中的字段有没有空的值
      *
-     * @param t 需要被检测的对象
-     * @param basePackageName  对象的类的基础包名
-     * @return  如果对象中有一个字段是空值，返回true(空字符串""判定为空值)
+     * @param t               需要判断的对象
+     * @param basePackageName 需要检测字段所在的包名，默认是com.shanglv51下
+     * @return
      */
-    public static <T> boolean checkFieldSEmpty(T t, String basePackageName) {
+    public static boolean checkFiledsIsEmpty(Object t, String basePackageName) {
+        return checkFiledsIsEmpty(t, basePackageName, null);
+    }
+
+    /**
+     * 判断一个model中的字段有没有空的值
+     *
+     * @param t            需要判断的对象
+     * @param ignoreFields 忽略的字段，对象类.字段名，例如A.a
+     * @return
+     */
+    public static boolean checkFiledsIsEmpty(Object t, String[] ignoreFields) {
+        String defaultBasePackageName = "com.shanglv51";
+        return checkFiledsIsEmpty(t, defaultBasePackageName, ignoreFields);
+    }
+
+    /**
+     * 判断一个model中的字段有没有空的值
+     *
+     * @param t               需要判断的对象
+     * @param basePackageName 对象中需要进入其内部继续判断的字段的基础包名
+     * @param ignoreFields    忽略的字段，对象类.字段名，例如A.a
+     * @return
+     */
+    public static boolean checkFiledsIsEmpty(Object t, String basePackageName, String[] ignoreFields) {
         if (t == null) {
             return true;
         }
         Field[] fields = t.getClass().getDeclaredFields();
+        if (ignoreFields != null && ignoreFields.length != 0) {
+            String[] _ignoreFields = Arrays.stream(ignoreFields).filter(s -> s.startsWith(t.getClass().getSimpleName())).toArray(value -> new String[value]);
+            fields = Arrays.stream(fields).filter(field -> {
+                for (String ignoreField : _ignoreFields) {
+                    return !ignoreField.equals(t.getClass().getSimpleName() + "." + field.getName());
+                }
+                return true;
+            }).toArray(value -> new Field[value]);
+        }
         for (Field field : fields) {
             field.setAccessible(true);
             Package _package = field.getType().getPackage();
@@ -43,7 +77,26 @@ public class CheckFields {
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 }
-                if (o == null || o.toString().length() == 0) {
+
+                if (o == null) {
+                    return true;
+                }
+                if ((o.getClass().isArray())) {
+                    for (int i = 0; i < Array.getLength(o); i++) {
+                        boolean flag = checkFiledsIsEmpty(Array.get(o, i), basePackageName, ignoreFields);
+                        if (flag == true) {
+                            return true;
+                        }
+                    }
+                } else if (Collection.class.isAssignableFrom(o.getClass())) {
+                    Collection collection = (Collection) o;
+                    for (Object o1 : collection) {
+                        boolean flag = checkFiledsIsEmpty(o1, basePackageName, ignoreFields);
+                        if(flag==true){
+                            return true;
+                        }
+                    }
+                } else if (o.toString().length() == 0) {
                     return true;
                 }
             } else {
@@ -52,7 +105,10 @@ public class CheckFields {
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 }
-                return checkFieldSEmpty(o);
+                boolean flag = checkFiledsIsEmpty(o, basePackageName, ignoreFields);
+                if(flag==true){
+                    return  true;
+                }
             }
         }
         return false;
